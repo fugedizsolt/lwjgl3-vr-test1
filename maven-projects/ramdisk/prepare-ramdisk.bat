ec660093ee4b8a41bce89992b546c5df8ce33df1
fuge <fugedi.zsolt@gmail.com>
2020-01-19 09:44:17 +0100

    eclipse fájlok kényelem miatt

--- maven-projects/ramdisk/prepare-ramdisk.bat
+++ maven-projects/ramdisk/prepare-ramdisk.bat
@@ -4,10 +4,10 @@ SET RAMDISK_PATH=R:\lwjgl3-vr-test1
 mkdir %RAMDISK_PATH%
 
 call mklink-folder.bat %RAMDISK_PATH%\src                          %ORIG_PATH%\src
-rem call mklink-folder.bat %RAMDISK_PATH%\.settings                    %ORIG_PATH%\.settings
+call mklink-folder.bat %RAMDISK_PATH%\.settings                    %ORIG_PATH%\.settings
 call mklink-file.bat   %RAMDISK_PATH%\log4j-config.xml             %ORIG_PATH%\log4j-config.xml
 call mklink-file.bat   %RAMDISK_PATH%\pom.xml                      %ORIG_PATH%\pom.xml
 call mklink-file.bat   %RAMDISK_PATH%\bild.bat                     %ORIG_PATH%\bild.bat
 call mklink-file.bat   %RAMDISK_PATH%\run1.bat                     %ORIG_PATH%\run1.bat
-rem call mklink-file.bat   %RAMDISK_PATH%\.classpath                   %ORIG_PATH%\.classpath
-rem call mklink-file.bat   %RAMDISK_PATH%\.project                     %ORIG_PATH%\.project
+call mklink-file.bat   %RAMDISK_PATH%\.classpath                   %ORIG_PATH%\.classpath
+call mklink-file.bat   %RAMDISK_PATH%\.project                     %ORIG_PATH%\.project
