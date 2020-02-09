d67bb19f907d62dac1397da5ec9c755c3162e4d0
fuge <fugedi.zsolt@gmail.com>
2020-02-09 19:54:28 +0100

    ezt fogom kitenni vivepro HMD-re

--- maven-projects/src/main/java/test/gl02/Renderer.java
+++ maven-projects/src/main/java/test/gl02/Renderer.java
@@ -21,7 +21,11 @@ public class Renderer
 	private static final float FOV = (float) Math.toRadians(60.0f);
 	private static final float Z_NEAR = 0.01f;
 	private static final float Z_FAR = 1000.f;
-	private Matrix4f allTransformMatrix;
+	private final Matrix4f allTransformMatrix = new Matrix4f();
+	private Matrix4f projectionMatrix;
+	private long prevRenderTimestamp = 0;
+
+	private long renderTimestampFromStart = 0;
 
 	private ShaderProgram shaderProgram = null;
 	private Mesh mesh = null;
@@ -48,33 +52,49 @@ public class Renderer
 			1.0f, 0.0f, -1.0f,
 			3.0f, 0.0f, -1.0f,
 			1.0f, 1.0f, -1.0f,
+
+			-990.0f,   0.0f, -900.0f,
+			 900.0f,   0.0f, -900.0f,
+			-900.0f, 900.0f, -900.0f,
+			 900.0f, 900.0f, -900.0f,
 		};
 		float[] colours = new float[]
 		{
-			1.0f, 0.0f, 0.0f, 
-			1.0f, 0.0f, 0.0f, 
-			1.0f, 0.0f, 0.0f,
+			0.7f, 0.2f, 0.2f, 
+			0.7f, 0.2f, 0.2f, 
+			0.7f, 0.2f, 0.2f,
 
-			0.0f, 1.0f, 0.0f, 
-			0.0f, 1.0f, 0.0f, 
-			0.0f, 1.0f, 0.0f,
+			0.2f, 0.7f, 0.2f, 
+			0.2f, 0.7f, 0.2f, 
+			0.2f, 0.7f, 0.2f,
+
+			0.8f, 0.8f, 0.95f, 
+			0.8f, 0.8f, 0.95f, 
+			0.8f, 0.8f, 0.95f,
+			0.8f, 0.8f, 0.95f,
 		};
-		int[] indices = new int[] { 0, 1, 2, 3, 4, 5, };
+		int[] indices = new int[] { 0,1,2, 3,4,5, 6,7,8,7,8,9 };
 		mesh = new Mesh( positionsInWorldSpace,colours,indices );
 
 		float aspectRatio = (float)windowWidth / windowHeight;
-		Matrix4f projectionMatrix = new Matrix4f().setPerspective( Renderer.FOV,aspectRatio,Renderer.Z_NEAR,Renderer.Z_FAR );
-
-		Vector3fc eye = new Vector3f( 0.0f,0.0f,3.0f );
-		Vector3fc center = new Vector3f( 0.0f,0.0f,0.0f );
-		Vector3fc up = new Vector3f( 0.0f,1.0f,0.0f );
-		Matrix4f viewMatrix = new Matrix4f().setLookAt( eye,center,up );
-
-		// Vclip = Mprojection x Mview x Mmodel x Vlocal
-		allTransformMatrix = projectionMatrix.mul( viewMatrix );
+		this.projectionMatrix = new Matrix4f().setPerspective( Renderer.FOV,aspectRatio,Renderer.Z_NEAR,Renderer.Z_FAR );
 	}
 
 	public void render()
+	{
+		long currentRenderTimestamp = System.currentTimeMillis();
+		if ( prevRenderTimestamp>0 )
+		{
+			renderWithElapsedTime( currentRenderTimestamp-prevRenderTimestamp );
+		}
+		else
+		{
+			renderWithElapsedTime( 0L );
+		}
+		prevRenderTimestamp = currentRenderTimestamp;
+	}
+
+	public void renderWithElapsedTime( long longDeltaTime )
 	{
 		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
@@ -83,6 +103,17 @@ public class Renderer
 //			glViewport( 0,0,window.getWidth(),window.getHeight() );
 //			window.setResized( false );
 //		}
+
+		renderTimestampFromStart += longDeltaTime;
+		double doubleDeltaTime = renderTimestampFromStart/500.0d;
+		Vector3fc eye = new Vector3f( 1.0f+(float)Math.sin( doubleDeltaTime )/5.0f,(float)Math.cos( doubleDeltaTime )/10.0f,3.0f );
+//		Vector3fc eye = new Vector3f( 0.0f,0.0f,3.0f );
+		Vector3fc center = new Vector3f( 1.0f,0.0f,0.0f );
+		Vector3fc up = new Vector3f( 0.0f,1.0f,0.0f );
+		Matrix4f viewMatrix = new Matrix4f().setLookAt( eye,center,up );
+
+		// Vclip = Mprojection x Mview x Mmodel x Vlocal
+		projectionMatrix.mul( viewMatrix,allTransformMatrix );
 
 		shaderProgram.bind();
 		shaderProgram.setUniform( VERTEX_SHADER_PARAM_TRANSFORM,allTransformMatrix );
@@ -107,4 +138,13 @@ public class Renderer
 		if ( mesh!=null )
 			mesh.cleanUp();
 	}
+
+//	public static void main( String[] args )
+//	{
+//		for ( int ic=0; ic<2000; ic+=10 )
+//		{
+//			float sinVal = (float)Math.sin( ic/100.0d )/10.0f;
+//			System.out.println( String.format( "ic=%d,sin(%f)",ic,sinVal ) );
+//		}
+//	}
 }
