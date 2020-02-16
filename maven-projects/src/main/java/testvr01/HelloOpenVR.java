cb755c1838f44ab9ea64ae81e9f9fcc78fef84e5
fuge <fugedi.zsolt@gmail.com>
2020-02-16 11:28:57 +0100

    tisztázott testvr01 prg

---
+++ maven-projects/src/main/java/testvr01/HelloOpenVR.java
@@ -0,0 +1,37 @@
+package testvr01;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class HelloOpenVR
+{
+	public static void main( String[] args ) throws Exception
+	{
+		new HelloOpenVR();
+	}
+
+	private HelloOpenVR() throws Exception
+	{
+		Path pathStop = Paths.get( "stop.dat" );
+		if ( Files.exists( pathStop )==true )
+			Files.delete( pathStop );
+
+		try ( ManagerOpenVR movr = new ManagerOpenVR() )
+		{
+			while ( true )
+			{
+				if ( Files.exists( pathStop )==true )
+					break;
+
+				movr.pollEvents();
+				movr.handleInputs();
+			}
+		}
+	}
+
+	public static void log( String msg,Object ... args )
+	{
+		System.out.println( String.format( msg,args ) );
+	}
+}
