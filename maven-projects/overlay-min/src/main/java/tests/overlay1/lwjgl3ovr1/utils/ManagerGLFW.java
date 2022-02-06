d162a0cc1b8cf615e7167d7df69665e9499ccb5c
fuge <fugedi.zsolt@gmail.com>
2022-02-06 13:41:30 +0100

    köztes állapot

---
+++ maven-projects/overlay-min/src/main/java/tests/overlay1/lwjgl3ovr1/utils/ManagerGLFW.java
@@ -0,0 +1,184 @@
+package tests.overlay1.lwjgl3ovr1.utils;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_X;
+import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
+import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
+import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
+import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
+import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
+import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
+import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
+import static org.lwjgl.glfw.GLFW.glfwGetVideoMode;
+import static org.lwjgl.glfw.GLFW.glfwInit;
+import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
+import static org.lwjgl.glfw.GLFW.glfwPollEvents;
+import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetKeyCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowPos;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowShouldClose;
+import static org.lwjgl.glfw.GLFW.glfwShowWindow;
+import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
+import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
+import static org.lwjgl.glfw.GLFW.glfwTerminate;
+import static org.lwjgl.glfw.GLFW.glfwWindowHint;
+import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_FALSE;
+import static org.lwjgl.opengl.GL11.GL_TRUE;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL11.glFinish;
+import static org.lwjgl.opengl.GL11.glFlush;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+import static org.lwjgl.opengl.GL11.*;
+
+import java.io.PrintStream;
+import java.nio.file.Path;
+
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWKeyCallback;
+import org.lwjgl.glfw.GLFWKeyCallbackI;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+
+import tests.overlay1.lwjgl3only.Main1;
+
+public class ManagerGLFW implements AutoCloseable
+{
+	private static boolean printCoordsSign = false;
+	public static boolean printCoords = false;
+
+	private static final int WIDTH = 300;
+	private static final int HEIGHT = 300;
+
+	private final long windowHandle;
+
+
+	public ManagerGLFW( PrintStream psGLFWError )
+	{
+		GLFWErrorCallback.createPrint( psGLFWError ).set();
+
+		// Initialize GLFW. Most GLFW functions will not work before doing this.
+		if ( !glfwInit() ) 
+		{
+			throw new IllegalStateException( "Unable to initialize GLFW" );
+		}
+
+		// Configure our window
+		glfwDefaultWindowHints(); // optional, the current window hints are already the default
+		glfwWindowHint( GLFW_VISIBLE,GL_FALSE ); // the window will stay hidden after creation
+		glfwWindowHint( GLFW_RESIZABLE,GL_TRUE ); // the window will be resizable
+
+		// Create the window
+		this.windowHandle = glfwCreateWindow( WIDTH,HEIGHT,"Hello World!",NULL,NULL );
+		if ( windowHandle == NULL )
+		{
+			throw new RuntimeException( "Failed to create the GLFW window" );
+		}
+
+		// Setup a key callback. It will be called every time a key is pressed, repeated or released.
+		GLFWKeyCallbackI callbackFunc = new GLFWKeyCallback()
+		{
+			@Override
+			public void invoke( long window,int key,int scancode,int action,int mods )
+			{
+				if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+				{
+					glfwSetWindowShouldClose( window,true ); // We will detect this in the rendering loop
+				}
+				if ( key == GLFW_KEY_X && action == GLFW_RELEASE )
+				{
+					if ( printCoordsSign==false )
+					{
+						Main1.log( "GLFW_KEY_X" );
+						printCoordsSign = true;
+						printCoords = true;
+					}
+				}
+			}
+		};
+		glfwSetKeyCallback( windowHandle,callbackFunc );
+
+		// Get the resolution of the primary monitor
+		GLFWVidMode vidmode = glfwGetVideoMode( glfwGetPrimaryMonitor() );
+		// Center our window
+		glfwSetWindowPos( windowHandle,(vidmode.width() - WIDTH) / 2,(vidmode.height() - HEIGHT) / 2 );
+
+		// Make the OpenGL context current
+		glfwMakeContextCurrent( windowHandle );
+		// Enable v-sync
+		glfwSwapInterval( 0 );
+
+		// Make the window visible
+		glfwShowWindow( windowHandle );
+
+		// This line is critical for LWJGL's interoperation with GLFW's
+		// OpenGL context, or any context that is managed externally.
+		// LWJGL detects the context that is current in the current thread,
+		// creates the GLCapabilities instance and makes the OpenGL
+		// bindings available for use.
+		GL.createCapabilities();
+
+		// Set the clear color
+		glClearColor( 1.0f,0.0f,0.0f,0.0f );
+	}
+
+	public void genTexture()
+	{
+		int m_nTextureIdEye1 = glGenTextures();
+		glBindTexture( GL_TEXTURE_2D,m_nTextureIdEye1 );
+		glTexParameteri( GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST ); // we also want to sample this texture later
+		glTexParameteri( GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST ); // we also want to sample this texture later
+		glTexImage2D( GL_TEXTURE_2D,0,GL_RGBA,WIDTH,HEIGHT,0,GL_RGBA,GL_UNSIGNED_BYTE,myjunkdata );
+	}
+
+	public void loop( Path pathStop ) throws InterruptedException
+	{
+		float countFrames = 0;
+		long tsStart = System.currentTimeMillis();
+		// Run the rendering loop until the user has attempted to close
+		// the window or has pressed the ESCAPE key.
+		while ( !glfwWindowShouldClose( windowHandle ) )
+		{
+//			if ( Files.exists( pathStop )==true )
+//				break;
+
+			glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT ); // clear the framebuffer
+
+			countFrames += 1.0f;
+
+//			glFinish();
+//			Thread.sleep( 1 );
+
+			glfwSwapBuffers( windowHandle ); // swap the color buffers
+
+//			glClearColor( 0,0,0,1 );
+//			glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+			glFlush();	// ez kell ide a doksi szerint
+			glFinish();
+
+			// Poll for window events. The key callback above will only be
+			// invoked during this call.
+			printCoords = false;
+			glfwPollEvents();
+		}
+		long tsDiff = System.currentTimeMillis()-tsStart;
+		Main1.log( String.format( "fps=%f (countFrames=%f)(tsDiff=%d)",1000*countFrames/tsDiff,countFrames,tsDiff ) );
+	}
+
+	@Override
+	public void close() throws Exception
+	{
+		// Release window and window callbacks
+		glfwFreeCallbacks( windowHandle );
+		glfwDestroyWindow( windowHandle );
+
+		// Terminate GLFW and release the GLFWerrorfun
+		glfwTerminate();
+		glfwSetErrorCallback( null ).free();
+	}
+}
---
+++ maven-projects/overlay-min/src/main/java/tests/overlay1/lwjgl3ovr1/utils/ManagerOpenVR.java
@@ -0,0 +1,154 @@
+package tests.overlay1.lwjgl3ovr1.utils;
+
+import java.nio.IntBuffer;
+import java.nio.LongBuffer;
+
+import org.lwjgl.openvr.OpenVR;
+import org.lwjgl.openvr.VR;
+import org.lwjgl.openvr.VREvent;
+import org.lwjgl.openvr.VRInput;
+import org.lwjgl.openvr.VROverlay;
+import org.lwjgl.openvr.VRSystem;
+import org.lwjgl.openvr.VRTextureBounds;
+import org.lwjgl.system.MemoryStack;
+import org.lwjgl.system.MemoryUtil;
+
+import tests.overlay1.lwjgl3ovr1.Main1;
+
+
+public class ManagerOpenVR implements AutoCloseable
+{
+	private final int openVRtoken;
+	private final VREvent event;
+	private int vrrc;				// return code from VR func
+	private long lHandleOverlay;	// handle from VR func
+
+	private final LongBuffer pHandleOverlay = MemoryUtil.memAllocLong( 1 );
+
+
+	public ManagerOpenVR()
+	{
+		try ( MemoryStack stack = MemoryStack.stackPush() )
+		{
+			this.event = VREvent.create();
+			this.openVRtoken = createOpenVR( stack );
+			listDevices( stack );
+
+			this.vrrc = VROverlay.VROverlay_CreateOverlay( "overlayKey1","overlayName1",pHandleOverlay );
+			this.lHandleOverlay = pHandleOverlay.get( 0 );
+			Main1.log( "VROverlay_CreateOverlay rc=%d vrHandle=%d",vrrc,this.lHandleOverlay );
+
+			this.vrrc = VROverlay.VROverlay_SetOverlayWidthInMeters( this.lHandleOverlay,0.3f );
+			Main1.log( "VROverlay_SetOverlayWidthInMeters rc=%d",vrrc );
+
+			this.vrrc = VROverlay.VROverlay_SetOverlayColor( this.lHandleOverlay,0.1f,0.8f,0.7f );
+			Main1.log( "VROverlay_SetOverlayColor rc=%d",vrrc );
+
+			VRTextureBounds pBounds = VRTextureBounds.malloc( stack );
+			pBounds.set( 0,0,1,1 );
+			this.vrrc = VROverlay.VROverlay_SetOverlayTextureBounds( this.lHandleOverlay,pBounds );
+			Main1.log( "VROverlay_SetOverlayTextureBounds rc=%d",vrrc );
+
+			this.vrrc = VROverlay.VROverlay_ShowOverlay( this.lHandleOverlay );
+			Main1.log( "VROverlay_ShowOverlay rc=%d",vrrc );
+		}
+	}
+
+	private int createOpenVR( MemoryStack stack )
+	{
+		Main1.log( "VR_IsRuntimeInstalled(%b)",VR.VR_IsRuntimeInstalled() );
+		Main1.log( "VR_RuntimePath(%s)",VR.VR_RuntimePath() );
+		Main1.log( "VR_IsHmdPresent(%b)",VR.VR_IsHmdPresent() );
+
+		IntBuffer peError = stack.mallocInt( 1 );
+
+		int token = 0;
+		try
+		{
+			token = VR.VR_InitInternal( peError,VR.EVRApplicationType_VRApplication_Scene );
+			int rc = peError.get( 0 );
+			if ( rc!=0 )
+			{
+				Main1.log( "INIT ERROR SYMBOL (%s)",VR.VR_GetVRInitErrorAsSymbol( rc ) );
+				Main1.log( "INIT ERROR  DESCR (%s)",VR.VR_GetVRInitErrorAsEnglishDescription( rc ) );
+				throw new RuntimeException( "VR_InitInternal peError.get( 0 ):" + rc );
+			}
+			if ( token==0 )
+				throw new RuntimeException( "token=0" );
+			Main1.log( "token=%d",token );
+
+			OpenVR.create( token );
+		}
+		catch ( Exception exc )
+		{
+			VR.VR_ShutdownInternal();
+			throw exc;
+		}
+		return token;
+	}
+
+	private void listDevices( MemoryStack stack )
+	{
+		IntBuffer peError = stack.mallocInt( 1 );
+
+		Main1.log( "Model Number (%s)",VRSystem.VRSystem_GetStringTrackedDeviceProperty( VR.k_unTrackedDeviceIndex_Hmd,VR.ETrackedDeviceProperty_Prop_ModelNumber_String,peError ) );
+		Main1.log( "Serial Number(%s)",VRSystem.VRSystem_GetStringTrackedDeviceProperty( VR.k_unTrackedDeviceIndex_Hmd,VR.ETrackedDeviceProperty_Prop_SerialNumber_String,peError ) );
+
+		int countBaseStations = 0;
+		for ( int ic=VR.k_unTrackedDeviceIndex_Hmd; ic<VR.k_unMaxTrackedDeviceCount; ic++ )
+		{
+			if ( VRSystem.VRSystem_IsTrackedDeviceConnected( ic )==true )
+			{
+				int trackedDeviceClass = VRSystem.VRSystem_GetTrackedDeviceClass( ic );
+				Main1.log( "id=%d,trackedDeviceClass=%d",ic,trackedDeviceClass );
+				Main1.log( "TrackingSystemName(%s)",VRSystem.VRSystem_GetStringTrackedDeviceProperty( ic,VR.ETrackedDeviceProperty_Prop_TrackingSystemName_String,peError ) );
+				Main1.log( "ModeLabel(%s)",VRSystem.VRSystem_GetStringTrackedDeviceProperty( ic,VR.ETrackedDeviceProperty_Prop_ModeLabel_String,peError ) );
+				Main1.log( "ModelNumber(%s)",VRSystem.VRSystem_GetStringTrackedDeviceProperty( ic,VR.ETrackedDeviceProperty_Prop_ModelNumber_String,peError ) );
+
+				if ( trackedDeviceClass==VR.ETrackedDeviceClass_TrackedDeviceClass_TrackingReference )
+					countBaseStations++;
+			}
+		}
+		Main1.log( "countBaseStations=%d",countBaseStations );
+
+		IntBuffer w = stack.mallocInt( 1 );
+		IntBuffer h = stack.mallocInt( 1 );
+		VRSystem.VRSystem_GetRecommendedRenderTargetSize( w,h );
+		Main1.log( "Recommended width : " + w.get( 0 ) );
+		Main1.log( "Recommended height: " + h.get( 0 ) );
+	}
+
+	public void pollEvents()
+	{
+		while ( true )
+		{
+			boolean bHasNext = VRSystem.VRSystem_PollNextEvent( this.event );
+			if ( bHasNext==false )
+				break;
+
+			int eventType = this.event.eventType();
+			int trackedDeviceIndex = this.event.trackedDeviceIndex();
+			Main1.log( "event trackedDeviceIndex(\t%03d) eventType(%d)",trackedDeviceIndex,eventType );
+		}
+	}
+
+	@Override
+	public void close() throws Exception
+	{
+		try
+		{
+			Main1.log( "calling VR_ShutdownInternal..." );
+			VR.VR_ShutdownInternal();
+			Main1.log( "VR_ShutdownInternal...end" );
+		}
+		catch ( Exception exc )
+		{
+			exc.printStackTrace();
+		}
+	}
+
+	public int getOpenVRtoken()
+	{
+		return openVRtoken;
+	}
+}
